de218ecf069bd5c69630f3028de64c0312b2099e
githubuiux <uiuxista@gmail.com>
2023-06-14 17:02:10 +0300

    Initial Commit

---
+++ InteractiveCharts/InteractiveChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveChartsApp.swift
+//  InteractiveCharts
+//
+//  Created by Levent on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
